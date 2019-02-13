1923f3f03826c0a82391c5afc34c6375eef6fc67
Guy Hershko <ghershko3@gmail.com>
2019-02-13 18:11:27 +0200

    Route

---
+++ Client/src/Components/DrowingPage/DrowingFile.js
@@ -0,0 +1,13 @@
+import React, { Component, Fragment } from 'react';
+
+class DrowingFile extends Component {
+  render() {
+    return (
+      <Fragment>
+        היי
+      </Fragment>
+    );
+  }
+}
+
+export default DrowingFile;
