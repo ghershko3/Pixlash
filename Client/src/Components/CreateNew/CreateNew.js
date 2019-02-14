40536766e7aab869de1a4f93dd6486c1ca8be423
Motola <Motola@DESKTOP-MIKJ13K>
2019-02-14 05:41:13 +0200

    add GetCounter code

--- Client/src/Components/CreateNew/CreateNew.js
+++ Client/src/Components/CreateNew/CreateNew.js
@@ -1,39 +1,64 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import logo from '../../logo.png';
 import { Route } from 'react-router-dom';
 import ReactDOM from 'react-dom';
 import QRCode from './QRCode'
 
 class CreateNew extends Component {
-    constructor(props){
-        super(props)
-        this.state={
-            value: false
-        }
+  constructor(props) {
+    super(props)
+    this.state = {
+      value: false,
+      count: 0
     }
-     render() {
-         const {value} = this.state
-        return (
-       <div>
+  }
+
+  GetCounter = async () => {
+    try {
+      debugger
+      const req = await fetch('/api/getClientsCount')
+      debugger
+      const json = await req.json()
+      debugger
+      this.setState({ count: json.count })
+    }
+    catch (err) {
+      console.log(err)
+    }
+  }
+
+  click = () => {
+    this.setState({ value: true }, this.GetCounter)
+  }
+
+
+  render() {
+    const { value, count } = this.state
+    return (
+      <div>
         <h1> You are going to be a part of a big picture!</h1>
         <form>
-  <label>
-    Name:
+          <label>
+            Name:
     <input type="text" name="name" />
-  </label>
-  <button
-      type='button'
-      onClick={() => { this.setState({value:true}) }}
-    >
-      Click Me!
+          </label>
+          <button
+            type='button'
+            onClick={this.click}
+          >
+            Click Me!
     </button>
-</form>
+        </form>
 
- { value ?  <QRCode /> : null }
+        {value &&
+          <Fragment>
+            <QRCode />
+            <h3>{count}</h3>
+          </Fragment>}
 
-  </div>
-        );
-    }
+      </div>
+    );
+  }
 }
 export default CreateNew;
 
